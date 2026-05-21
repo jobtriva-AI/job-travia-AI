9f7817c08960b28da89a2503df39a1c070be7a94
jobtriva-AI <saeedixit439@gmail.com>
2026-05-21 11:02:49 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,4 @@ import streamlit as st
 
 st.title("Job Triva AI Recruiter")
 
-st.write("App is running successfully 🚀")
-
-job = st.text_area("Job Description")
-file = st.file_uploader("Upload Resume PDF")
-
-if file:
-    st.success("File uploaded successfully!")
-
-if job:
-    st.info("Job description received!")
+st.success("App deployed successfully 🚀")
