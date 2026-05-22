e9e8d9cfd869ac0ead2bd148d250c1c194ebb2df
jobtriva-AI <saeedixit439@gmail.com>
2026-05-22 10:23:51 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,5 @@
 import streamlit as st
 
-st.set_page_config(page_title="Job Triva ATS", layout="wide")
+st.title("Job Triva ATS")
 
-st.title("📊 Job Triva ATS")
-
-st.success("App is running successfully 🚀")
-
-st.write("If you see this, Streamlit is working correctly.")
-
-job = st.text_area("Job Description")
-
-file = st.file_uploader("Upload Resume PDF")
-
-if file:
-    st.info("File uploaded successfully")
-
-if job:
-    st.info("Job description added")
+st.success("App is running 🚀")
